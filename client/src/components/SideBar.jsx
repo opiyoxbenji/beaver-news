5a6c8aecc0b41f9a98729c56f906c175d3819cbc
opiyos <opiyosbenji@gmail.com>
2024-03-11 18:34:26 +0300

    git status

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -21,7 +21,7 @@ const SideBar = () => {
 							className='my-5 border-b-2 border-spacing-2 px-4'>
 							<h4 className='font-medium mb-2'>{blog.title}</h4>
 							<Link
-								to='/'
+								to={`/blogs/${blog.id}`}
 								className='text-base pb-2 hover:text-purple-600 inline-flex items-center py-1'>
 								Read more <FaArrowRight className='mt-1 ml-2' />
 							</Link>
