8f388938b0b98b8609aeb4d894ad62149caee531
opiyos <opiyosbenji@gmail.com>
2024-03-11 19:18:13 +0300

    minor changes

--- client/src/pages/SingleBlog.jsx
+++ client/src/pages/SingleBlog.jsx
@@ -16,7 +16,7 @@ const SingleBlog = () => {
 
 	return (
 		<div>
-			<div className='py-40 bg-black text-center text-white px-4'>
+			<div className='py-40 bg-blue-950 text-center text-white px-4'>
 				<h2 className='text-5xl lg:text-7xl leading-snug font-bold mb-5'>
 					Single Blog Page
 				</h2>
