e4f7b38aaeb16748c8973e35885d027c21879b53
Jassocc <132377359+Jassocc@users.noreply.github.com>
2024-03-14 10:27:02 +0200

    Add comments to vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'// Importing defineConfig function from Vite
+import react from '@vitejs/plugin-react' // Importing the Vite plugin for React
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // Using the Vite React plugin
 })
