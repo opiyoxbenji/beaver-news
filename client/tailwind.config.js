c9e20c9cc450f7e8116ee8e665de821e41abdb5c
Jassocc <132377359+Jassocc@users.noreply.github.com>
2024-03-14 10:28:35 +0200

    Added comments tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	// Define the content paths for PurgeCSS to analyze
 	content: ['./index.html', './src/**/*.{html,js,ts,jsx,tsx}'],
+	// Theme customization
 	theme: {
 		extend: {
+			 // Extend the default font family with Rubik and fallback to sans-serif
 			fontFamily: {
 				primary: ['Rubik', 'sans-serif'],
 			},
 		},
 	},
+	// Plugins array, currently empty
 	plugins: [],
 };
