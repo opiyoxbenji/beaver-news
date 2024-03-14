9530aab8b00fc4cf030a39e26685fb9396afd80d
Jassocc <132377359+Jassocc@users.noreply.github.com>
2024-03-14 10:24:31 +0200

    Added comments index.js

--- server/index.js
+++ server/index.js
@@ -1,26 +1,26 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const blogs = require('./api/blogsData.json')
-const port = process.env.PORT || 5000;
+const blogs = require('./api/blogsData.json') // Importing JSON data containing blogs
+const port = process.env.PORT || 5000; // Setting port, either from environment variable or default to 5000
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors()) // Enable CORS for cross-origin requests
+app.use(express.json()) // Parse JSON request bodies
 
 app.get('/', (req, res) => {
-    res.send("Blog server is running!")
+    res.send("Blog server is running!") // Sending a simple response to confirm server is running
 });
 
 app.get('/blogs', (req, res) => {
-  res.send(blogs)
+  res.send(blogs // Sending all blogs data as response
 })
 app.get('/blogs/:id', (req, res) => {
-  const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id); // Extracting blog ID from URL parameter
   // console.log(id)
   const blog = blogs.filter(b => b.id === id);
   // console.log(blog)
-  res.send(blog)
+  res.send(blog) // Sending the found blog as response
 })
 
 app.listen(port, () => {
